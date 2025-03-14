790dd2eb36a18879ecc86ae45ca6ea97ae30814e
Sauvik Dolui <sauvikdolui@gmail.com>
2025-03-14 22:03:29 +0530

    Update ShortestWordDistanceII.swift

--- Array/ShortestWordDistanceII.swift
+++ Array/ShortestWordDistanceII.swift
@@ -36,3 +36,39 @@
         return minDistance
     }
 }
+
+// Trying out solution
+class WordDistance {
+    var index: [String: [Int]] = [:]
+    init(_ words: [String]) {
+        for (i, word) in words.enumerated() {
+            index[word, default:[]].append(i)
+        }
+    }
+    
+    func shortest(_ word1: String, _ word2: String) -> Int {
+        guard let word1Pos = index[word1],
+        let word2Pos = index[word2] else {
+            fatalError("Invalid entry")
+        }
+        
+        var i = 0, j = 0
+        var minDistance = Int.max
+        while i < word1Pos.count && j < word2Pos.count {
+            minDistance = min(minDistance, 
+                             abs(word1Pos[i] - word2Pos[j])
+                        )
+            if word1Pos[i] < word2Pos[j] {
+                i += 1
+            } else if word2Pos[j] < word1Pos[i] {
+               j += 1 
+            }
+        }
+        return minDistance
+    }
+}
+
+let wordsDict = ["practice", "makes", "perfect", "coding", "makes"]
+let wordDistance = WordDistance(wordsDict)
+print(wordDistance.shortest("coding", "practice")) // Output: 3
+print(wordDistance.shortest("makes", "coding"))   // Output: 1
