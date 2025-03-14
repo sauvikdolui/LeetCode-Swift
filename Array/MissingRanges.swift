f74596d35ff06ef5c26728970df12c65b63ece19
Sauvik Dolui <sauvikdolui@gmail.com>
2025-03-14 17:16:54 +0530

    Update MissingRanges.swift

--- Array/MissingRanges.swift
+++ Array/MissingRanges.swift
@@ -36,3 +36,49 @@
         }
     }
 }
+
+
+infix operator **
+extension Int {
+    static func ** (_ num: Int, _ power: Int) -> Int {
+        var result = 1
+        for _ in 0..<power {
+            result *= num
+        }
+        return result
+    }
+}
+
+// let nums = [0, 1, 3, 50, 75], lower = 0, upper = 99
+// Output: ["2", "4->49", "51->74", "76->99"]
+// let nums = [1, 2, 3, 4, 5], lower = 1, upper = 5
+// let nums = [1, 10], lower = 1, upper = 10
+// let nums = [-10, 0, 10], lower = 1, upper = 5
+// let nums = [1, 2, 4, 5], lower = 1, upper = 5
+// let nums: [Int] = [], lower: Int = 1, upper: Int = 3
+// let nums = [5], lower = 5, upper = 5
+// let nums = [3, 4, 5, 8, 9], lower = 1, upper = 10
+// let nums = [-3, -2, 2, 3], lower = -5, upper = 5
+let nums = [-(10**6), 0, 10**6], lower = -(10**6), upper = 10**6
+print(missingRange(nums, lower, upper))
+
+func missingRange(_ nums: [Int], _ lower: Int, _ upper: Int) -> [String] {
+
+    var result = [String]()
+    var prev = lower - 1
+    
+    // Iterate through nums with an extra iteration for the upper bound
+    for i in 0...nums.count {
+        let num = (i < nums.count) ? nums[i] : upper + 1
+        if num - prev == 2 {
+            // Single missing number
+            result.append("\(prev + 1)")
+        } else if num - prev > 2 {
+            // Range of missing numbers
+            result.append("\(prev + 1)->\(num - 1)")
+        }
+        prev = num
+    }
+    
+    return result
+}
